d6f6c89658372842ba5993d7f5837b0413607693
Joel Ostos <joelostoscastro@gmail.com>
2024-07-13 00:19:12 -0500

    Adding some macros and container functions

---
+++ src/codes/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define pii pair<int, int> 
+#define vi vector<int> 
+#define vb vector<bool> 
+#define vpi vector<pair<int, int>> 
+#define vvi vector<vector<int>>
+#define ff first
+#define ss second
+#define all(container) container.begin(), container.end()
+
+void solve()
+{
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t; cin >> t;
+  while(t--) solve();
+}
